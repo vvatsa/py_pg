4f29b8e97fc28f51ab017cdea33694e65e857007
Vishal Vatsa <vishal@vishal.io>
2025-09-25 07:30:19 +0100

    tidy

--- init.sql
+++ init.sql
@@ -1,8 +1,8 @@
 CREATE EXTENSION multicorn;
-create SERVER dds FOREIGN DATA WRAPPER multicorn options ( wrapper 'dds.dummy_fdw.DummyFDW');
+create SERVER dummy FOREIGN DATA WRAPPER multicorn options ( wrapper 'dds.dummy_fdw.DummyFDW');
 CREATE FOREIGN TABLE test (
     test character varying,
     test2 character varying
-    ) server dds options(
+    ) server dummy options(
         num_rows '100'
     );
